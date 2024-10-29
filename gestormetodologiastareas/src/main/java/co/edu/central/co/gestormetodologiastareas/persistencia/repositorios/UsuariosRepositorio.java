775cd1cf00ef60b9f899b3f55c3dc306168cb5e4
juancangrejo04 <juancangrejo04@gmail.com>
2024-10-29 15:59:05 -0500

    primer y segundo casos de uso

---
+++ gestormetodologiastareas/src/main/java/co/edu/central/co/gestormetodologiastareas/persistencia/repositorios/UsuariosRepositorio.java
@@ -0,0 +1,4 @@
+package co.edu.central.co.gestormetodologiastareas.persistencia.repositorios;
+
+public interface UsuariosRepositorio {
+}
---
+++ gestormetodologiastareas/src/main/java/co/edu/central/co/gestormetodologiastareas/servicios/UsuariosServicios.java
@@ -0,0 +1,4 @@
+package co.edu.central.co.gestormetodologiastareas.servicios;
+
+public class UsuariosServicios {
+}
