7063f7fc5229d763727100222aba207c96d6c4c7
juancangrejo04 <juancangrejo04@gmail.com>
2024-10-29 13:56:43 -0500

    Se agrega el proyecto

---
+++ gestormetodologiastareas/src/main/java/co/edu/central/co/gestormetodologiastareas/GestormetodologiastareasApplication.java
@@ -0,0 +1,13 @@
+package co.edu.central.co.gestormetodologiastareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestormetodologiastareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestormetodologiastareasApplication.class, args);
+	}
+
+}
---
+++ gestormetodologiastareas/src/test/java/co/edu/central/co/gestormetodologiastareas/GestormetodologiastareasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.central.co.gestormetodologiastareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestormetodologiastareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
